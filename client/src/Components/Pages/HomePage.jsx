b5a250ec9c55d8aa28525e60254885ef71462fbc
TuqayW <101931875+TuqayW@users.noreply.github.com>
2023-05-28 07:17:12 -0700

    added nav

--- client/src/Components/Pages/HomePage.jsx
+++ client/src/Components/Pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Footer from '../../Components/Footer'
+import Nav from '../Nav'
 const HomePage = () => {
   return (
     <div>
-      
+         <Nav/>
         <Footer/>
     </div>
   )
